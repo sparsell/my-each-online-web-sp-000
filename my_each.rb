cfe13ca110b7ba9e366d35e71101d6cccd68e8ea
Sonja Parsell <hello@sparsell.com>
2020-03-27 15:26:49 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    i = 0
+    while i < array.length
+      yield (array[i])
+      i += 1
+    end
+    array
+  else
+    "hey..."
+  end
+
+end
+
+array = [1, 2, 3, 4]
+my_each(array) do |i|
+  return i
 end
